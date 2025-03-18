eebc7791f0094c7c677f15c581ee117dbc1b3243
FlokiTV <dawysonw@gmail.com>
2025-03-18 10:57:33 -0300

    feat: update user services

--- src/api/users/users.controller.ts
+++ src/api/users/users.controller.ts
@@ -7,23 +7,78 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { UpdateUserDto, updateUserSchema } from './dto/update-user.dto';
 import { AuthGuard, Payload } from '@/guards/auth.guard';
+import { ApiResponse } from '@nestjs/swagger';
+import { Validate } from 'nestjs-typebox';
+import { Type } from '@sinclair/typebox';
+import { selectUserSchema } from './dto/select-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
   @UseGuards(AuthGuard)
   @Get('/me')
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+    content: {
+      'application/json': {
+        examples: {
+          unauthorized: {
+            value: {
+              statusCode: 401,
+              message: 'Unauthorized',
+            },
+          },
+        },
+      },
+    },
+  })
+  @Validate({
+    response: Type.Omit(selectUserSchema, ['password']),
+  })
   me(@Request() req: Payload) {
     const user = req.user;
-    return this.usersService.me(user.id);
+    return this.usersService.getById(user.id);
   }
 
   @UseGuards(AuthGuard)
   @Patch('/me')
-  update(@Request() req: Payload, @Body() updateUserDto: UpdateUserDto) {
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+    content: {
+      'application/json': {
+        examples: {
+          unauthorized: {
+            value: {
+              statusCode: 401,
+              message: 'Unauthorized',
+            },
+          },
+        },
+      },
+    },
+  })
+  @Validate({
+    response: Type.Omit(selectUserSchema, ['password']),
+    request: [
+      {
+        type: 'body',
+        schema: Type.Omit(updateUserSchema, [
+          'id',
+          'password',
+          'lastSignAt',
+          'createdAt',
+          'updatedAt',
+        ]),
+      },
+    ],
+  })
+  update(@Body() updateUserDto: UpdateUserDto, @Request() req: Payload) {
     const user = req.user;
-    // return this.usersService.updateMe(user.id, updateUserDto);
+    return this.usersService.updatadeById(user.id, updateUserDto);
   }
 }
--- src/api/users/users.service.ts
+++ src/api/users/users.service.ts
@@ -1,8 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { DRIZZLE } from '@/drizzle/drizzle.module';
+import { DrizzleDB } from '@/drizzle/types/drizzle';
+import {
+  ForbiddenException,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { user } from './entities/users.entity';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
 export class UsersService {
-  async me(id: number) {
-    return '';
+  constructor(@Inject(DRIZZLE) private db: DrizzleDB) {}
+
+  async getById(id: number) {
+    const me = await this.db.query.user.findFirst({
+      where: (user, { eq }) => eq(user.id, id),
+    });
+    if (!me) {
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.NO_CONTENT,
+          message: 'User not found',
+        },
+        HttpStatus.CONFLICT,
+      );
+    }
+    return me;
+  }
+
+  async updatadeById(id: number, updateUserDto: UpdateUserDto) {
+    // Check if updateUserDto is empty
+    if (!Object.keys(updateUserDto).length)
+      throw new ForbiddenException('Access Denied');
+
+    await this.getById(id);
+
+    try {
+      const [success] = await this.db
+        .update(user)
+        .set(updateUserDto)
+        .where(eq(user.id, Number(id)))
+        .returning();
+
+      if (!success) throw new Error('User not found');
+
+      return success;
+    } catch (error) {
+      console.error('Error:', error);
+      throw new ForbiddenException('Access Denied');
+    }
   }
 }
