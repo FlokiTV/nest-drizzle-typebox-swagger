5805ef9dfdc50b121e00d20db9fb343ec5b3408c
FlokiTV <dawysonw@gmail.com>
2025-03-18 07:59:53 -0300

    fix: add password and last login

--- src/api/users/entities/users.entity.ts
+++ src/api/users/entities/users.entity.ts
@@ -2,11 +2,10 @@ import * as t from 'drizzle-orm/sqlite-core';
 
 export const user = t.sqliteTable('user', {
   id: t.int().primaryKey({ autoIncrement: true }),
-  name: t.text('name'),
-  email: t.text('email'),
-  phone: t.text('phone'),
-  date: t.text('date'),
-  address: t.text('address'),
+  name: t.text('name').notNull(),
+  email: t.text('email').notNull(),
+  password: t.text('password').notNull(),
+  lastSignAt: t.integer('lastSignAt').default(0),
   createdAt: t.integer('createdAt').$defaultFn(() => Date.now()),
   updatedAt: t
     .integer('updatedAt')
