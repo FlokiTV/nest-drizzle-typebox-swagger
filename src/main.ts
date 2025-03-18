8fe14412ff89c05050f477235dd42fd90333844d
FlokiTV <dawysonw@gmail.com>
2025-03-18 08:05:16 -0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,53 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { configureNestJsTypebox } from 'nestjs-typebox';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
+
+configureNestJsTypebox({
+  patchSwagger: true,
+  setFormats: true,
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('')
+    .setVersion('1.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'JWT',
+      description: 'JWT Token for authentication',
+    })
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  app.use(
+    '/scalar',
+    apiReference({
+      spec: {
+        content: document,
+      },
+    }),
+  );
+
+  await app.listen(port, () => {
+    console.log('-----------------------');
+    console.log('🎉 - Servidor Online !');
+    console.log('\x1b[34m%s\x1b[0m', '🔗 - http://localhost:3000');
+    console.log('\x1b[33m%s\x1b[0m', '📃 - http://localhost:3000/api');
+    console.log('\x1b[33m%s\x1b[0m', '📃 - http://localhost:3000/scalar');
+    console.log('-----------------------');
+  });
 }
 bootstrap();
