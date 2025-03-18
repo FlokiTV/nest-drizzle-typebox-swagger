b5856561292ee35b9b7b28179366c914a4d4f6fe
FlokiTV <dawysonw@gmail.com>
2025-03-18 11:18:50 -0300

    feat: add validations

--- src/api/users/dto/create-user.dto.ts
+++ src/api/users/dto/create-user.dto.ts
@@ -1,6 +1,16 @@
-import { Static } from '@sinclair/typebox';
+import { Static, Type } from '@sinclair/typebox';
 import { createInsertSchema } from 'drizzle-typebox';
 import { user } from '../entities/users.entity';
 
-export const creatUserSchema = createInsertSchema(user);
+export const creatUserSchema = createInsertSchema(user, {
+  name: Type.String({
+    minLength: 1,
+  }),
+  email: Type.String({
+    format: 'email',
+  }),
+  password: Type.String({
+    minLength: 8,
+  }),
+});
 export type CreateUserDto = Static<typeof creatUserSchema>;
--- src/api/users/dto/update-user.dto.ts
+++ src/api/users/dto/update-user.dto.ts
@@ -1,6 +1,17 @@
-import { Static } from '@sinclair/typebox';
+import { Static, Type } from '@sinclair/typebox';
 import { createUpdateSchema } from 'drizzle-typebox';
 import { user } from '../entities/users.entity';
 
-export const updateUserSchema = createUpdateSchema(user);
+export const updateUserSchema = createUpdateSchema(user, {
+  name: Type.Optional(
+    Type.String({
+      minLength: 1,
+    }),
+  ),
+  email: Type.Optional(
+    Type.String({
+      format: 'email',
+    }),
+  ),
+});
 export type UpdateUserDto = Static<typeof updateUserSchema>;
